b3c247f107f3fd1687922b6f73b880b1b2d093cd
ilkay55 <akinsamsun@gmail.com>
2022-12-29 21:42:57 +0300

    Day42-Added

---
+++ src/day42_Exceptions/ThrowsKeyword.java
@@ -0,0 +1,25 @@
+package day42_Exceptions;
+
+public class ThrowsKeyword {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("-------- Test 1------------");
+
+        System.out.println("Hello");
+        Thread.sleep(3000);
+        System.out.println("Cydeo");
+
+        System.out.println("-------- Test 1 Completed------------");
+
+
+        System.out.println("-------- Test 2------------");
+
+        System.out.println("Hello");
+        Thread.sleep(3000);
+        System.out.println("EU-10");
+
+        System.out.println("-------- Test 2 Completed------------");
+
+    }
+
+}
